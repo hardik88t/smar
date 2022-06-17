0d90510756a6ac38756f192ab34437b9958f9f2a
hardik88t <77385955+hardik88t@users.noreply.github.com>
2022-06-17 15:41:44 +0530

    working save & delete all : )

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,29 +1,58 @@
-// function saveLink(){
-//     let link1 = document.getElementById("input-el").value;
-//     // localStorage.setItem("input", input);
-//     console.log(link1);
-//     // console.log("Hello");
-// }
-let links = ["https://github.com/hardik88t"]
+
+let links = ["https://www.youtube.com/watch?v=dQw4w9WgXcQ"];
 const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
-const printAt = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const ulEl = document.getElementById("ul-el");
+const messageDiv = document.getElementById("message");
+const localData = localStorage.getItem("smar");
+let printed = false;
+
+if(localData == undefined){
+    localStorage.setItem("smar", JSON.stringify(links));
+}
+
+if(printed == false){
+    printLinks();
+}
 
 inputBtn.addEventListener("click", function() {
+    links = JSON.parse(localStorage.getItem("smar"));
+    if(inputEl.value === "") {
+        console.log("Nothing to add");
+        messageDiv.innerHTML = 'Nothing to add';
+        inputEl.focus();
+    }
+    else{
+        messageDiv.innerHTML = "";
+        links.push(inputEl.value);
+        inputEl.focus();
+        localStorage.setItem("smar", JSON.stringify(links));
+        printLinks();
+    }
+});
 
-    links.push(inputEl.value);
-    inputEl.value = "";
-    printAt.innerHTML = null;
+deleteBtn.addEventListener("dblclick", function(){
+    arr = []
+    localStorage.removeItem("smar");
+    localStorage.setItem("smar", JSON.stringify(arr));
+    ulEl.innerHTML = "";
+})
+
+function printLinks(){
+    links = JSON.parse(localStorage.getItem("smar"));
     let linkItems = "";
     for (let i = 0; i < links.length; i++) {
-
-        // linkItems += "<li><a target='_blank' href='"+links[i]+"'>"+links[i]+"</a> </li>"
         linkItems += `
-        <li>
-            <a target='_blank' href='${links[i]}'>
-                ${links[i]}
-            </a> 
-        </li>`
+                        <li>
+                            <a target='_blank' href='${links[i]}'>
+                                ${links[i]}
+                            </a> 
+                        </li>
+                    `
     }
-    printAt.innerHTML = linkItems;
-});
+    ulEl.innerHTML = linkItems;
+    inputEl.value = "";
+    printed = true;
+}
+
